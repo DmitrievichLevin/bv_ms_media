fffc466e9c920498bf362e0dc5dc5a8a99de5e2b
dmitrievichlevin <jhowar39@emich.edu>
2024-10-19 02:38:10 -0700

    commented body check for testing

--- src/media_microservice/square/payment_link.py
+++ src/media_microservice/square/payment_link.py
@@ -20,8 +20,6 @@ def request_payment_link(event: dict[str, Any]) -> dict[str, str]:
     Returns:
         body: response body containing url.
 
-    Raises:
-        KeyError: Incorrect request body.
     """
     session = requests.Session()
     retry = Retry(total=3, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
@@ -40,31 +38,31 @@ def request_payment_link(event: dict[str, Any]) -> dict[str, str]:
     line_items, shipping_address, buyer_email_address, __phone, source_id = itemgetter(
         'line_items', "shipping_address", "buyer_email_address", "phone", "source_id")(event_body)
 
-    if all(getattr(shipping_address, k, False)
-           for k in ["address_line_1", "administrative_district_level_1", "country", "first_name", "last_name", "locality", "postal_code"]):
+    # if all(isinstance(getattr(shipping_address, k, False), str)
+    #        for k in ["address_line_1", "administrative_district_level_1", "country", "first_name", "last_name", "locality", "postal_code"]):
 
-        body = {
-            'idempotency_key': idempotency_key,
-            "source_id": source_id,
-            "autocomplete": True,
-            "location_id": location_id,
-            "line_items": line_items,
-            "shipping_address": shipping_address,
-            "accept_partial_authorization": False,
-            "buyer_email_address": buyer_email_address,
-            "countryCode": 'US',
-            "currencyCode": 'USD',
-        }
+    body = {
+        'idempotency_key': idempotency_key,
+        "source_id": source_id,
+        "autocomplete": True,
+        "location_id": location_id,
+        "line_items": line_items,
+        "shipping_address": shipping_address,
+        "accept_partial_authorization": False,
+        "buyer_email_address": buyer_email_address,
+        "countryCode": 'US',
+        "currencyCode": 'USD',
+    }
 
-        response = session.post('https://connect.squareup.com/v2/payments',
-                                headers={'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json'}, json=body)
-        # Log response for debugging
-        logging.debug(response.json())
+    response = session.post('https://connect.squareup.com/v2/payments',
+                            headers={'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json'}, json=body)
+    # Log response for debugging
+    logging.debug(response.json())
 
-        response.raise_for_status()
+    response.raise_for_status()
 
-        pay_link: dict[str, str] = response.json()
+    pay_link: dict[str, str] = response.json()
 
-        return pay_link
-    else:
-        raise KeyError("Missing expected keys in request body.")
+    return pay_link
+    # else:
+    #     raise KeyError("Missing expected keys in request body.")
