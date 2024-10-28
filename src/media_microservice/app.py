9915f73e1fa6ff099adfc62b958543dddbf397c7
dmitrievichlevin <jhowar39@emich.edu>
2024-10-28 00:16:41 -0700

    fixed created_at column

--- src/media_microservice/app.py
+++ src/media_microservice/app.py
@@ -1,4 +1,5 @@
 """Lambda Handler"""
+import json as _json
 import logging
 from typing import Any
 
@@ -48,7 +49,7 @@ def lambda_handler(
                         "Access-Control-Allow-Origin": "*",
                         "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
                     },
-                    "body": body,
+                    "body": _json.dumps(body),
                 }
             else:
                 return {
